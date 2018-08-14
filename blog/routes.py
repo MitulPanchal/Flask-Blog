b9a484e63b0d51cdee10d916ae01de4652ebe9f2
MitulPanchal <imitulpanchal@gmail.com>
2018-08-14 13:13:32 +0530

    Add Blog page

--- blog/routes.py
+++ blog/routes.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image 
 from flask import render_template, url_for, flash, redirect, request
 from blog.models import User, Post
-from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from blog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from blog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -99,3 +99,11 @@ def account():
     image_file = url_for('static', filename = 'profile/' + current_user.image_file)
     return render_template('account.html', title='Account', image_file = image_file, form = form)
 
+@app.route("/posts/new", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        flash('Your Post is Created', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title="New Post", form=form)
